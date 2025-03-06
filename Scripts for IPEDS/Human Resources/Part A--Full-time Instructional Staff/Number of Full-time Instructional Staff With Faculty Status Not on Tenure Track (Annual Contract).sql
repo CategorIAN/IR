697be9f87ddc7cec1b81540fe596ef90ec768f94
Kessler <ikessler@carroll.edu>
2025-03-06 16:03:27 -0700

    ..

---
+++ Scripts for IPEDS/Human Resources/Part A--Full-time Instructional Staff/Number of Full-time Instructional Staff With Faculty Status Not on Tenure Track (Annual Contract).sql
@@ -0,0 +1,47 @@
+SELECT IPEDS_RACE_ETHNIC_DESC AS Race_Ethnicity,
+        [PRO] AS Professors,
+        [ASO] AS Associate_Professors,
+        [ASR] AS Assistant_Professors,
+        [INS] AS Instructors,
+        [Unknown] AS No_Academic_Rank
+FROM (SELECT IPEDS_RACE_ETHNIC_DESC,
+             ISNULL(POS_EEO_RANK, 'Unknown') as POS_EEO_RANK,
+             COUNT(*) AS HR_COUNT
+      FROM PERSTAT
+               JOIN PERSON ON PERSTAT.PERSTAT_HRP_ID = PERSON.ID
+               JOIN POSITION ON PERSTAT.PERSTAT_PRIMARY_POS_ID = POSITION.POSITION_ID
+               JOIN Z01_ALL_RACE_ETHNIC_W_FLAGS AS RACE ON PERSTAT_HRP_ID = RACE.ID
+      WHERE PERSTAT_END_DATE IS NULL
+        AND PERSTAT_START_DATE <= '2024-11-01'
+        AND PERSTAT_STATUS = 'FT'
+        AND POS_CLASS = 'FAC'
+        AND PERSTAT_TENURE_TYPE = 'N'
+        AND GENDER = 'M'
+      GROUP BY IPEDS_RACE_ETHNIC_DESC, POS_EEO_RANK) AS X
+PIVOT (
+    SUM(HR_COUNT) FOR POS_EEO_RANK IN ([PRO], [ASO], [ASR], [INS], [Unknown])
+) AS Y
+
+SELECT IPEDS_RACE_ETHNIC_DESC AS Race_Ethnicity,
+        [PRO] AS Professors,
+        [ASO] AS Associate_Professors,
+        [ASR] AS Assistant_Professors,
+        [INS] AS Instructors,
+        [Unknown] AS No_Academic_Rank
+FROM (SELECT IPEDS_RACE_ETHNIC_DESC,
+             ISNULL(POS_EEO_RANK, 'Unknown') as POS_EEO_RANK,
+             COUNT(*) AS HR_COUNT
+      FROM PERSTAT
+               JOIN PERSON ON PERSTAT.PERSTAT_HRP_ID = PERSON.ID
+               JOIN POSITION ON PERSTAT.PERSTAT_PRIMARY_POS_ID = POSITION.POSITION_ID
+               JOIN Z01_ALL_RACE_ETHNIC_W_FLAGS AS RACE ON PERSTAT_HRP_ID = RACE.ID
+      WHERE PERSTAT_END_DATE IS NULL
+        AND PERSTAT_START_DATE <= '2024-11-01'
+        AND PERSTAT_STATUS = 'FT'
+        AND POS_CLASS = 'FAC'
+        AND PERSTAT_TENURE_TYPE = 'N'
+        AND GENDER = 'F'
+      GROUP BY IPEDS_RACE_ETHNIC_DESC, POS_EEO_RANK) AS X
+PIVOT (
+    SUM(HR_COUNT) FOR POS_EEO_RANK IN ([PRO], [ASO], [ASR], [INS], [Unknown])
+) AS Y
