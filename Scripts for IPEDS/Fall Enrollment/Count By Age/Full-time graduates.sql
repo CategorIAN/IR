edd8930255b35a7cebbe9316ae4d96214c0beb39
Kessler <ikessler@carroll.edu>
2025-03-12 15:42:54 -0600

    ..

---
+++ Scripts for IPEDS/Fall Enrollment/Count By Age/Full-time graduates.sql
@@ -0,0 +1,25 @@
+SELECT AGE_CATEGORY,
+       [M],
+       [F]
+FROM (SELECT DISTINCT STUDENT_ID,
+                      GENDER,
+                      CASE
+                          WHEN STUDENT_AGE < 18 THEN 'Under 18'
+                          WHEN STUDENT_AGE BETWEEN 18 AND 19 THEN '18-19'
+                          WHEN STUDENT_AGE BETWEEN 20 AND 21 THEN '20-21'
+                          WHEN STUDENT_AGE BETWEEN 22 AND 24 THEN '22-24'
+                          WHEN STUDENT_AGE BETWEEN 25 AND 29 THEN '25-29'
+                          WHEN STUDENT_AGE BETWEEN 30 AND 34 THEN '30-34'
+                          WHEN STUDENT_AGE BETWEEN 35 AND 39 THEN '35-39'
+                          WHEN STUDENT_AGE BETWEEN 40 AND 49 THEN '40-49'
+                          WHEN STUDENT_AGE BETWEEN 50 AND 64 THEN '50-64'
+                          WHEN STUDENT_AGE >= 65 THEN '65 and over'
+                          WHEN STUDENT_AGE IS NULL THEN 'Age unknown/unreported'
+                          END AS AGE_CATEGORY
+      FROM STUDENT_ENROLLMENT_VIEW AS SEV
+               JOIN PERSON ON STUDENT_ID = PERSON.ID
+               JOIN STUDENT_TERMS_VIEW AS STV ON SEV.STUDENT_ID = STV.STTR_STUDENT AND SEV.ENROLL_TERM = STV.STTR_TERM
+      WHERE SEV.ENROLL_TERM = '2024FA'
+        AND STV.STTR_ACAD_LEVEL = 'GR'
+        AND STV.STTR_STUDENT_LOAD IN ('F', 'O')) AS X
+PIVOT (COUNT(STUDENT_ID) FOR GENDER IN ([M], [F])) AS Y
---
+++ Scripts for IPEDS/Fall Enrollment/Graduate Students/Enrollment CIP Code 14.0000.sql
@@ -0,0 +1,67 @@
+SELECT RACE,
+       [Full-Time],
+       [Part-Time]
+FROM (SELECT STTR_STUDENT,
+             RACE.IPEDS_RACE_ETHNIC_DESC AS RACE,
+             CASE
+                 WHEN STTR_STUDENT_LOAD IN ('F', 'O') THEN 'Full-Time'
+                 ELSE 'Part-Time'
+                 END                     AS STATUS
+
+      FROM STUDENT_TERMS_VIEW
+               JOIN PERSON ON STTR_STUDENT = PERSON.ID
+               JOIN Z01_ALL_RACE_ETHNIC_W_FLAGS AS RACE ON STUDENT_TERMS_VIEW.STTR_STUDENT = RACE.ID
+               JOIN (SELECT *
+                     FROM (SELECT STUDENT_ID,
+                                  STP_ACADEMIC_PROGRAM,
+                                  STP_PROGRAM_TITLE,
+                                  STP_START_DATE,
+                                  STP_CURRENT_STATUS,
+                                  ROW_NUMBER() OVER (PARTITION BY STUDENT_ID
+                                      ORDER BY STP_START_DATE DESC) AS rn
+                           FROM STUDENT_ACAD_PROGRAMS_VIEW
+                           WHERE STP_CURRENT_STATUS != 'Changed Program'
+                             AND STP_START_DATE <= '2024-12-12') ranked
+                     WHERE rn = 1)
+          AS SAPV ON STUDENT_TERMS_VIEW.STTR_STUDENT = SAPV.STUDENT_ID
+      JOIN ACAD_PROGRAMS ON SAPV.STP_ACADEMIC_PROGRAM = ACAD_PROGRAMS_ID
+      WHERE STUDENT_TERMS_VIEW.STTR_TERM = '2024FA'
+        AND SAPV.STP_CURRENT_STATUS NOT IN ('Not Returned', 'Withdrawn', 'Did Not Enroll')
+        AND STTR_ACAD_LEVEL = 'GR'
+        AND ACPG_CIP LIKE '14%'
+        AND GENDER = 'M') AS X
+PIVOT (COUNT(STTR_STUDENT) FOR STATUS IN ([Full-Time], [Part-Time])) AS Y
+
+SELECT RACE,
+       [Full-Time],
+       [Part-Time]
+FROM (SELECT STTR_STUDENT,
+             RACE.IPEDS_RACE_ETHNIC_DESC AS RACE,
+             CASE
+                 WHEN STTR_STUDENT_LOAD IN ('F', 'O') THEN 'Full-Time'
+                 ELSE 'Part-Time'
+                 END                     AS STATUS
+
+      FROM STUDENT_TERMS_VIEW
+               JOIN PERSON ON STTR_STUDENT = PERSON.ID
+               JOIN Z01_ALL_RACE_ETHNIC_W_FLAGS AS RACE ON STUDENT_TERMS_VIEW.STTR_STUDENT = RACE.ID
+               JOIN (SELECT *
+                     FROM (SELECT STUDENT_ID,
+                                  STP_ACADEMIC_PROGRAM,
+                                  STP_PROGRAM_TITLE,
+                                  STP_START_DATE,
+                                  STP_CURRENT_STATUS,
+                                  ROW_NUMBER() OVER (PARTITION BY STUDENT_ID
+                                      ORDER BY STP_START_DATE DESC) AS rn
+                           FROM STUDENT_ACAD_PROGRAMS_VIEW
+                           WHERE STP_CURRENT_STATUS != 'Changed Program'
+                             AND STP_START_DATE <= '2024-12-12') ranked
+                     WHERE rn = 1)
+          AS SAPV ON STUDENT_TERMS_VIEW.STTR_STUDENT = SAPV.STUDENT_ID
+      JOIN ACAD_PROGRAMS ON SAPV.STP_ACADEMIC_PROGRAM = ACAD_PROGRAMS_ID
+      WHERE STUDENT_TERMS_VIEW.STTR_TERM = '2024FA'
+        AND SAPV.STP_CURRENT_STATUS NOT IN ('Not Returned', 'Withdrawn', 'Did Not Enroll')
+        AND STTR_ACAD_LEVEL = 'GR'
+        AND ACPG_CIP LIKE '14%'
+        AND GENDER = 'F') AS X
+PIVOT (COUNT(STTR_STUDENT) FOR STATUS IN ([Full-Time], [Part-Time])) AS Y
